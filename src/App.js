bec7b379a2cbbf171d4f3e418afad3d41b3e8398
RafixOOO <smoktbg@gmail.com>
2023-09-06 15:42:42 +0200

    eventer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useState } from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-function App() {
+const options = [
+  {
+    name: 'Groups',
+    scroll: true,
+    backdrop: true,
+  },
+];
+
+function ColorSchemesExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="">Eventer</Navbar.Brand>
+          <Nav className="me-auto">
+            {options.map((props, idx) => (
+        <OffCanvasExample key={idx} {...props} />
+      ))}
+            <Nav.Link href="#features">Events</Nav.Link>
+          </Nav>
+          <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+          <a href="#login">Sign in</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
-export default App;
+export default ColorSchemesExample;
+
+
+
+function OffCanvasExample({ name, ...props }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const toggleShow = () => setShow((s) => !s);
+
+  return (
+    <>
+      <Nav.Link onClick={toggleShow}>
+        {name}
+      </Nav.Link>
+      <Offcanvas show={show} onHide={handleClose} {...props}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Groups</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
