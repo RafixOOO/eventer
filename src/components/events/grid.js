8f1d5d885bc01081807e157aa15c75276b91f1c6
RafixOOO <smoktbg@gmail.com>
2023-09-07 11:03:40 +0200

    eventer

---
+++ src/components/events/grid.js
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function GridExample() {
+  return (
+        <div className="AppContainer">
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 8 }).map((_, idx) => (
+        <Col key={idx}>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+    </div>
+  );
+}
+
+export default GridExample;
