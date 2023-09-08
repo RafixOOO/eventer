f70700f5f154c2a5b94612582656e604879ee38c
RafixOOO <smoktbg@gmail.com>
2023-09-08 14:54:21 +0200

    eventer

--- src/components/events/grid.js
+++ src/components/events/grid.js
@@ -61,7 +61,7 @@ longer.`;
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Card.Img variant="top" style={{ maxHeight: '400px' }} src='src\components\events\img\img2.jpg' className='img-fluid' />
+        <Card.Img variant="top" style={{ maxHeight: '400px' }} src={'./img/img2.jpg'} className='img-fluid' />
         <Linkify>{text}</Linkify>
         </Modal.Body>
         <Modal.Footer>
