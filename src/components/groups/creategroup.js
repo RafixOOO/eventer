9c45d1035e8a2a2494412545b2f87d6f2b89b07a
RafixOOO <smoktbg@gmail.com>
2023-09-27 11:18:22 +0200

    eventer

--- src/components/groups/creategroup.js
+++ src/components/groups/creategroup.js
@@ -1,24 +1,129 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function TextControlsExample() {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [isPublic, setIsPublic] = useState(false);
+  const [kolo, setKolo] = useState(null);
+  const [User, setUser] = useState(null);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleIsPublicChange = (e) => {
+    setIsPublic(e.target.value === 'true');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Tutaj możesz wykonać odpowiednie operacje na danych (np. wysłać na serwer itp.)
+    console.log('Name:', name);
+    console.log('Description:', description);
+    console.log('Is Public:', isPublic);
+    create();
+  };
+
+  const create = () => {
+    axios
+      .post(`http://localhost:8080/api/Kolo/Add`, {
+        nazwa: name,
+        opis: description,
+        status: isPublic,
+        image: "58b6edb8-8503-4e13-9773-1b63803e0ca1_s.jpg",
+      },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('authdata')}`,
+          },
+        })
+      .then(response => {
+        const responseData = response.data;
+        setKolo(responseData);
+        current();
+      })
+      .catch((error) => {
+        console.log(error);
+        sessionStorage.removeItem('authdata');
+        window.location.reload();
+      });
+
+  }
+
+  const current = () => {
+    axios.get('http://localhost:8080/api/User/Current', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${sessionStorage.getItem('authdata')}`,
+      },
+    })
+      .then(response => {
+        const responseData = response.data;
+        setUser(responseData);
+        connect();
+      })
+      .catch(error => {
+        console.error(error);
+        sessionStorage.removeItem('authdata');
+        window.location.reload();
+      });
+  }
+
+  const connect = () => {
+    axios
+      .post(`http://localhost:8080/api/KoloUser/Add`, {
+        eventsAdder: true,
+        manageUser: true,
+        permissionAdmin: true,
+        koloIdKola: kolo,
+        userIdUser: User,
+        chatWriter: true,
+        confirm: true,
+        badges: 0
+      },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('authdata')}`,
+          },
+        })
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+        sessionStorage.removeItem('authdata');
+        window.location.reload();
+      });
+
+  }
+
   return (
     <Form>
       <Form.Group className="mb-3">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" required/>
+        <Form.Control type="text" value={name} onChange={handleNameChange} required />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" required>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" value={description} onChange={handleDescriptionChange} required>
         <Form.Label>Description</Form.Label>
         <Form.Control as="textarea" rows={3} />
       </Form.Group>
-      <Form.Select aria-label="Default select example">
-      <option value="false">not visible</option>
-      <option value="true">visible</option>
-    </Form.Select>
-    <br />
-      <Button variant="primary">Create</Button>
+      <Form.Select aria-label="Default select example" value={isPublic ? 'true' : 'false'} onChange={handleIsPublicChange}>
+        <option value="false">private</option>
+        <option value="true">public</option>
+      </Form.Select>
+      <br />
+      <Button variant="primary" onClick={handleSubmit}>Create</Button>
     </Form>
   );
 }
--- src/components/groups/groups.js
+++ src/components/groups/groups.js
@@ -2,53 +2,98 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
-import Logo from './logo.svg';
 import Nav from 'react-bootstrap/Nav';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
 
 function DefaultExample() {
-    return (
-        <>
-        <ListGroup as="ol">
-            <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-            >
-                <Image style={{ maxWidth: '100%', height: 'auto', maxHeight: '25px', }} src={Logo} roundedCircle />
-                <div className="ms-2 me-auto">
-                    <div className="fw-bold"><Nav.Link href="/groups">Subheading</Nav.Link></div>
-                </div>
-                <Badge bg="danger" pill>
-                    14
-                </Badge>
-            </ListGroup.Item>
-            <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-            >
-                <Image style={{ maxWidth: '100%', height: 'auto', maxHeight: '25px', }} src={Logo} roundedCircle />
-                <div className="ms-2 me-auto">
-                    <div style={{ maxWidth: '100%' }} className="fw-bold"><Nav.Link href="/groups">Subheading</Nav.Link></div>
-                </div>
-                <Badge bg="danger" pill>
-                    14
-                </Badge>
-            </ListGroup.Item>
-            <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-start"
-            >
-                <Image style={{ maxWidth: '100%', height: 'auto', maxHeight: '25px', }} src={Logo} roundedCircle />
-                <div className="ms-2 me-auto">
-                    <div className="fw-bold"><Nav.Link href="/groups">Subheading</Nav.Link></div>
-                </div>
-                <Badge bg="danger" pill>
-                    1
-                </Badge>
-            </ListGroup.Item>
-        </ListGroup>
-        </>
-    );
+
+    const [Email, setEmail] = useState('');
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Wywołaj pobieranie danych użytkownika po zalogowaniu
+        axios.get('http://localhost:8080/api/User/Current', {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('authdata')}`,
+          },
+        })
+          .then(response => {
+            const responseData = response.data;
+            setEmail(responseData.email)
+          })
+          .catch(error => {
+            console.error(error);
+            sessionStorage.removeItem('authdata');
+            
+          });
+      }, []);
+
+      useEffect(() => {
+        // Wywołaj pobieranie danych użytkownika po zalogowaniu
+        axios.get(`http://localhost:8080/api/KoloUser/FindByUser?Person=${Email}`, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('authdata')}`,
+          },
+        })
+          .then(response => {
+            const responseData = response.data;
+            setData(responseData);
+          })
+          .catch(error => {
+            console.error(error);
+            sessionStorage.removeItem('authdata');
+            
+          });
+      }, [Email]);
     
+    
+     const AllItems = () => {
+      data.forEach(item => {
+        axios.get(`http://localhost:8080/api/Image/download/${item.koloIdKola.image}`, {
+          headers: {
+            Accept: 'application/octet-stream',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionStorage.getItem('authdata')}`,
+          },
+          responseType: 'blob',
+        })
+        .then((response) => {
+          const blob = new Blob([response.data], { type: 'image/jpeg' });
+          const imageUrl = URL.createObjectURL(blob);
+          item.koloIdKola.image = imageUrl;
+        })
+        .catch((error) => {
+          console.error(error);
+          sessionStorage.removeItem('authdata');
+        });
+      
+      return (
+        <>
+              <ListGroup.Item
+                as="li"
+                key={item.koloIdKola.id}
+                className="d-flex justify-content-between align-items-start"
+              >
+                <Image
+                  style={{ maxWidth: '100%', height: 'auto', maxHeight: '25px' }}
+                  src={item.koloIdKola.image}
+                  roundedCircle
+                />
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold"><Nav.Link href="/groups">{item.koloIdKola.nazwa}</Nav.Link></div>
+                </div>
+                <Badge bg="danger" pill>
+                  {item.badges}
+                </Badge>
+              </ListGroup.Item>
+        </>
+      );
+    });
 }
+return <AllItems />;
+    }
 
 export default DefaultExample;
